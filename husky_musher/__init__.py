cbfb3b24454319a096d6f45e23d0adacb89e406c
Tom Thorogood <tom@tomthorogood.com>
2021-10-15 17:31:41 -0700

    Remove unneeded code

--- husky_musher/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from id3c.logging.config import load_config
-
-
-def configure_logger(filename):
-    logger = logging.getLogger('gunicorn.error').getChild('app')
-    with open(filename, "rb") as file:
-        logging.config.dictConfig(load_config(file))
